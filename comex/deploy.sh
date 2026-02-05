1f045ee1e7a3ed77e74d19a07a63f26dae4c7756
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-02-05 11:47:21 +0200

    simpler deploy script and htaccess for caching

--- comex/deploy.sh
+++ comex/deploy.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-# Sync HTML files to public directory
-rsync -av index.html silver.html gold.html kesor@ssh.kesor.net:~/kesor.net/comex/
+# Sync public files
+rsync -av index.html silver.html gold.html .htaccess silver/ gold/ kesor@ssh.kesor.net:~/kesor.net/comex/
 
-# Sync data directories
-rsync -av silver/ kesor@ssh.kesor.net:~/kesor.net/comex/silver/
-rsync -av gold/ kesor@ssh.kesor.net:~/kesor.net/comex/gold/
-
-# Sync scripts to private directory
-rsync -av fetch.sh fetch_gold.sh cleanup_empty.sh update_and_deploy.sh cookies.txt kesor@ssh.kesor.net:~/comex/
+# Sync private scripts
+rsync -av fetch.sh fetch_gold.sh cleanup_empty.sh update_and_deploy.sh kesor@ssh.kesor.net:~/comex/
 
 echo "Deployed to https://kesor.net/comex/"
