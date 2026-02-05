515f7d0fe253839c0a892f716cc08fe2538c4283
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-02-05 10:37:05 +0200

    no delete in deploy needed anymore

--- comex/deploy.sh
+++ comex/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Sync HTML files to public directory
-rsync -av --delete index.html silver.html gold.html kesor@ssh.kesor.net:~/kesor.net/comex/
+rsync -av index.html silver.html gold.html kesor@ssh.kesor.net:~/kesor.net/comex/
 
 # Sync data directories
-rsync -av --delete silver/ kesor@ssh.kesor.net:~/kesor.net/comex/silver/
-rsync -av --delete gold/ kesor@ssh.kesor.net:~/kesor.net/comex/gold/
+rsync -av silver/ kesor@ssh.kesor.net:~/kesor.net/comex/silver/
+rsync -av gold/ kesor@ssh.kesor.net:~/kesor.net/comex/gold/
 
 # Sync scripts to private directory
 rsync -av fetch.sh fetch_gold.sh cleanup_empty.sh update_and_deploy.sh cookies.txt kesor@ssh.kesor.net:~/comex/
