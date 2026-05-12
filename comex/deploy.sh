7a8d102f61bab2cf8d7feac3ed08c8853656b399
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-05-12 15:49:21 +0300

    more data

--- comex/deploy.sh
+++ comex/deploy.sh
@@ -3,6 +3,6 @@
 rsync -av .htaccess *.html *.css *.js silver gold ssh.kesor.net:kesor.net/comex/
 
 # Sync private scripts
-rsync -av fetch.sh fetch_gold.sh cleanup_empty.sh update_and_deploy.sh kesor@ssh.kesor.net:~/comex/
+rsync -av fetch.sh cleanup_empty.sh update_and_deploy.sh kesor@ssh.kesor.net:~/comex/
 
 echo "Deployed to https://kesor.net/comex/"
